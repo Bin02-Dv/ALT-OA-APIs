e300b4812068ff34260a037f6b7d42a702e1faaa
Bin02-Dv <alaminbinahmad78@gmail.com>
2024-04-18 07:31:23 +0100

    16th commit

--- ALSProject/settings.py
+++ ALSProject/settings.py
@@ -27,10 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['altonlineacademy.onrender.com', 'localhost:8000']
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://127.0.0.1:3000',
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
