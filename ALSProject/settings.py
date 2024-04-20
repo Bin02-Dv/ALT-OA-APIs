737083cb508a2575881ac021f86ba4ec01c9bf1b
Bin02-Dv <alaminbinahmad78@gmail.com>
2024-04-20 13:24:40 +0100

    11th commit

--- ALSProject/settings.py
+++ ALSProject/settings.py
@@ -28,14 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['altonlineacademy.onrender.com', 'localhost', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
+    'https://alphatech-school.vercel.app',
     'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://alphatech-school.vercel.app'
+    'http://127.0.0.1:3000'
 ]
-
-CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
@@ -53,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,11 +127,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'ALSApp.AuthApiModel'
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
